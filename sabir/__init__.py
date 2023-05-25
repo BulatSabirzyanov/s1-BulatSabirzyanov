5b0675396fb6d923853e08d96e7e1d1a23b1823c
sabir <sabirzyanov429@gmail.com>
2023-05-25 16:12:38 +0300

    1

--- sabir/__init__.py
+++ sabir/__init__.py
@@ -8,13 +8,13 @@ app.secret_key = 'why would I tell you my secret key?'
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.sqlite3'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['UPLOAD_FOLDER'] = 'C:/Users/sabir/PycharmProjects/PythonFlaskMySQLApp_Part8'
+app.config['UPLOAD_FOLDER'] = 'C:/Users/sabir/PycharmProjects/s1-BulatSabirzyanov'
 db = SQLAlchemy(app)
 
 pageLimit = 5
 
 
-from sabir.models import User
+from sabir.models import User,BucketList,Like
 from sabir.routes import *
 
 
